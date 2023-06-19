0478a34b804031862b1809c3805a90dbea2721ea
PHP3p <1183162527@qq.com>
2023-06-19 11:32:04 +0800

    qiuckly open soft

---
+++ dev.bat
@@ -0,0 +1,9 @@
+start /d"D:\phpstudy\Extensions\redis3.0.504" redis-server.exe
+start /d"D:\phpstudy\COM" phpstudy_pro.exe
+start /d"D:\Program Files (x86)\Tencent\WeChat" WeChat.exe
+start /d"C:\Users\wangyang\AppData\Local\Postman" Postman.exe  
+start /d"D:\Program Files (x86)\OpenVPN\bin" openvpn-gui.exe
+start /d"D:\Program Files (x86)\JetBrains\PhpStorm 2021.1.2\bin" phpstorm64.exe
+start /d"D:\phpstudy\Extensions\MySQL5.7.26\bin" mysqld.exe
+start /d"D:\phpstudy\Extensions\Nginx1.15.11" nginx.exe
+start /d"D:\Program Files (x86)\PremiumSoft\Navicat Premium 15" navicat.exe
